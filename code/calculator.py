a31d9fc84c91f13d3777443c1f708ba44fd85026
Komes Pispol <90035701@study.curtincollege.edu.au>
2025-05-01 11:12:42 +0800

    Add: implemented get_generation

--- code/calculator.py
+++ code/calculator.py
@@ -36,4 +36,18 @@ def get_lucky_animal(lucky_number):
 
 
 def get_generation(year):
-    ...
+    generation = ""
+    if year in range(1901, 1946):
+        generation = "Silent Generation"
+    elif year in range(1946, 1965):
+        generation = "Baby Boomers"
+    elif year in range(1965, 1980):
+        generation = "Generation X"
+    elif year in range(1980, 1995):
+        generation = "Millennials"
+    elif year in range(1995, 2010):
+        generation = "Generation Z"
+    elif year in range(2010, 2025):
+        generation = "Generation Alpha"
+
+    return generation
