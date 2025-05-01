7200c088ae04e83ca44caad4ccbf7a5c8a92ad3a
Komes Pispol <90035701@study.curtincollege.edu.au>
2025-05-01 08:56:19 +0800

    Refactor: made all test case messages of test calculator module more comprehensible

--- code/test_calculator.py
+++ code/test_calculator.py
@@ -22,14 +22,14 @@ class TestCalculator(unittest.TestCase):
                ValueError,
                ValueError]
         
-        msg = ["Intended Case",
-               "Invalid day",
-               "Invalid month",
-               "Invalid year",
-               "Invalid day and month",
-               "Invalid month and year",
-               "Invalid day and year",
-               "Invalid day, month, and year"]
+        msg = ["Case: Intended",
+               "Case: Invalid day",
+               "Case: Invalid month",
+               "Case: Invalid year",
+               "Case: Invalid day and month",
+               "Case: Invalid month and year",
+               "Case: Invalid day and year",
+               "Case: Invalid day, month, and year"]
         
         for i in range(len(expected)):
             self.assertEqual(expected[i], calculator.calculate_lucky_number(birthday[i]), msg[i])
@@ -50,19 +50,19 @@ class TestCalculator(unittest.TestCase):
                     "Turtle", 
                     ""]
         
-        msg = ["== 1",
-               "== 2",
-               "== 3",
-               "== 4",
-               "== 5",
-               "== 6",
-               "== 7",
-               "== 8",
-               "== 9",
-               "== 11",
-               "== 22",
-               "== 33",
-               "Not in range 1-9 inclusive and not 11, 22, and 33"]
+        msg = ["Case: Lucky number == 1",
+               "Case: Lucky number == 2",
+               "Case: Lucky number == 3",
+               "Case: Lucky number == 4",
+               "Case: Lucky number == 5",
+               "Case: Lucky number == 6",
+               "Case: Lucky number == 7",
+               "Case: Lucky number == 8",
+               "Case: Lucky number == 9",
+               "Case: Lucky number == 11",
+               "Case: Lucky number == 22",
+               "Case: Lucky number == 33",
+               "Case: Lucky number not in range 1-9 inclusive and not 11, 22, and 33"]
         
         for i in range(len(expected)):
             self.assertEqual(expected[i], calculator.get_lucky_animal(lucky_number[i]), msg[i])
@@ -76,12 +76,12 @@ class TestCalculator(unittest.TestCase):
                     "Generation Z",
                     "Generation Alpha"]
         
-        msg = ["1901 <= year <= 1945",
-               "1946 <= year <= 1964",
-               "1965 <= year <= 1979",
-               "1980 <= year <= 1994",
-               "1995 <= year <= 2009",
-               "2010 <= year <= 2024"]
+        msg = ["Case: 1901 <= year <= 1945",
+               "Case: 1946 <= year <= 1964",
+               "Case: 1965 <= year <= 1979",
+               "Case: 1980 <= year <= 1994",
+               "Case: 1995 <= year <= 2009",
+               "Case: 2010 <= year <= 2024"]
         
         for i in range(len(expected)):
             self.assertEqual(expected[i], calculator.get_generation(year[i]), msg[i])
