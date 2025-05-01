408a32dbccab30942a8883d823015d14f6914caf
Komes Pispol <90035701@study.curtincollege.edu.au>
2025-05-01 10:55:17 +0800

    Add: implemented calculate_lucky_number

--- code/calculator.py
+++ code/calculator.py
@@ -1,5 +1,18 @@
-def calculate_lucky_number(birthday):
-    ...
+from data_structures import Date
+import logic
+
+def calculate_lucky_number(birthday: Date):
+    date_string = str(birthday.day) + str(birthday.month) + str(birthday.year)
+    lucky_number = 0
+    for char in date_string:
+        lucky_number += int(char)
+    if not logic.is_master_number(lucky_number):
+        tmp_lucky_number = 0
+        for char in str(lucky_number):
+            tmp_lucky_number += int(char)
+        lucky_number = tmp_lucky_number
+
+    return lucky_number
 
 def get_lucky_animal(lucky_number):
     ...
