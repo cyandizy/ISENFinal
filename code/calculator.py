51c7decfa335fbd317580ee33c60c57304dd0b66
Komes Pispol <90035701@study.curtincollege.edu.au>
2025-05-01 11:33:45 +0800

    Fix: calculate_lucky_number to handle invalid date

--- code/calculator.py
+++ code/calculator.py
@@ -2,6 +2,13 @@ from data_structures import Date
 import logic
 
 def calculate_lucky_number(birthday: Date):
+    if birthday.day not in range(1, 32):
+        return ValueError("Day value must be from 1 to 31 inclusive.")
+    elif birthday.month not in range(1, 13):
+        return ValueError("Month value must be from 1 to 12 inclusive.")
+    elif birthday.year not in range(1901, 2025):
+        return ValueError("Year value must be from 1901 to 2024 inclusive")
+    
     date_string = str(birthday.day) + str(birthday.month) + str(birthday.year)
     lucky_number = 0
     for char in date_string:
