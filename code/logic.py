487a38105d86ee25068b786403682815995d3b34
Komes Pispol <90035701@study.curtincollege.edu.au>
2025-05-01 09:06:53 +0800

    Update: implemented all submodules for logic module

--- code/logic.py
+++ code/logic.py
@@ -1,5 +1,14 @@
+import calculator
+
 def same_luck(birthday1, birthday2):
-    ...
+    if (calculator.calculate_lucky_number(birthday1) ==
+        calculator.calculate_lucky_number(birthday2)):
+        return True
+    else:
+        return False
 
 def is_master_number(lucky_number):
-    ...
+    if lucky_number in [11, 22, 33]:
+        return True
+    else:
+        return False
