33064cd07178c3c17a0a9ede32ccdac1bcb74a57
Komes Pispol <90035701@study.curtincollege.edu.au>
2025-05-01 15:44:15 +0800

    Update: changed test_calculate_lucky_number to handle exceptions better

--- code/test_calculator.py
+++ code/test_calculator.py
@@ -4,14 +4,14 @@ import unittest
 
 class TestCalculator(unittest.TestCase):
     def test_calculate_lucky_number(self):
-        birthday = [Date(10, 3, 2006),
-                    Date(0, 2, 2001),
-                    Date(10, 0, 1999),
-                    Date(20, 4, 9999),
-                    Date(-1, 0, 2004),
-                    Date(4, 99, 2193),
-                    Date(111, 12, 9),
-                    Date(99, 99, 9999)]
+        birthday = [(10, 3, 2006),
+                    (0, 2, 2001),
+                    (10, 0, 1999),
+                    (20, 4, 9999),
+                    (-1, 0, 2004),
+                    (4, 99, 2193),
+                    (111, 12, 9),
+                    (99, 99, 9999)]
         
         expected = [3,
                ValueError,
@@ -32,7 +32,10 @@ class TestCalculator(unittest.TestCase):
                "Case: Invalid day, month, and year"]
         
         for i in range(len(expected)):
-            self.assertEqual(expected[i], calculator.calculate_lucky_number(birthday[i]), msg[i])
+            if expected[i] == Exception:
+                self.assertRaises(expected[i], calculator.calculate_lucky_number(birthday[i]), msg[i])
+            else:
+              self.assertEqual(expected[i], calculator.calculate_lucky_number(birthday[i]), msg[i])
 
     def test_get_lucky_animal(self):
         lucky_number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 0]
