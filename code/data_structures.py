742ba25dc8c64a60f7461576d86b539e3bf80a6e
Komes Pispol <90035701@study.curtincollege.edu.au>
2025-05-01 15:37:57 +0800

    Refactor: ValueError in calculate_lucky_number is now raised in Date class

--- code/data_structures.py
+++ code/data_structures.py
@@ -3,3 +3,9 @@ class Date:
         self.day = day
         self.month = month
         self.year = year
+        if self.day not in range(1, 32):
+            return ValueError("Day value must be from 1 to 31 inclusive.")
+        elif self.month not in range(1, 13):
+            return ValueError("Month value must be from 1 to 12 inclusive.")
+        elif self.year not in range(1901, 2025):
+            return ValueError("Year value must be from 1901 to 2024 inclusive")
