496cc0b8362b298a45e757fec6bf9b9580c38f6f
Komes Pispol <90035701@study.curtincollege.edu.au>
2025-05-02 15:19:46 +0800

    Fix: format month to handle keyerror

--- code/data_structures.py
+++ code/data_structures.py
@@ -5,20 +5,24 @@ class Date:
         self.year = year
         
         if self.day not in range(1, 32):
-            return ValueError("Day value must be from 1 to 31 inclusive.")
+            raise ValueError("Day value must be from 1 to 31 inclusive.")
         elif self.month not in range(1, 13):
-            return ValueError("Month value must be from 1 to 12 inclusive.")
+            raise ValueError("Month value must be from 1 to 12 inclusive.")
         elif self.year not in range(1901, 2025):
-            return ValueError("Year value must be from 1901 to 2024 inclusive")
+            raise ValueError("Year value must be from 1901 to 2024 inclusive")
         
         if isinstance(self.month, str):
             self.month = self.format_month(self.month)
 
-    def format_month(self):
+    def format_month(self, month: str):
+        month = month.lower()
         month_dict = {
-            "January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6,
-            "July": 7, "August": 8, "September": 9, "October": 10, "November": 11, "December": 12,
-            "Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "Jun": 6, "Jul": 7,
-            "Aug": 8, "Sep": 9, "Sept": 9, "Oct": 10, "Nov": 11, "Dec": 12
+            "january": 1, "february": 2, "march": 3, "april": 4, "may": 5, "june": 6,
+            "july": 7, "august": 8, "september": 9, "october": 10, "november": 11, "december": 12,
+            "jan": 1, "feb": 2, "mar": 3, "apr": 4, "jun": 6, "jul": 7,
+            "aug": 8, "sep": 9, "sept": 9, "oct": 10, "nov": 11, "dec": 12
         }
-        return month_dict[self.month]
+        if month not in month_dict.keys():
+            raise ValueError
+
+        return month_dict[month]
