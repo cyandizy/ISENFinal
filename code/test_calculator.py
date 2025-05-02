6327e1c81c45de8ccdc3c555ea65a4b180138f6a
Komes Pispol <90035701@study.curtincollege.edu.au>
2025-05-02 10:34:32 +0800

    Chore: fixed indentation for files

--- code/test_calculator.py
+++ code/test_calculator.py
@@ -35,7 +35,7 @@ class TestCalculator(unittest.TestCase):
             if expected[i] == Exception:
                 self.assertRaises(expected[i], calculator.calculate_lucky_number(birthday[i]), msg[i])
             else:
-              self.assertEqual(expected[i], calculator.calculate_lucky_number(birthday[i]), msg[i])
+                self.assertEqual(expected[i], calculator.calculate_lucky_number(birthday[i]), msg[i])
 
     def test_get_lucky_animal(self):
         lucky_number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 0]
