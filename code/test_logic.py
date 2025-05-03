65667c527c41ed67e57163e0f5a45055c16cf285
Komes Pispol <90035701@study.curtincollege.edu.au>
2025-05-03 16:53:22 +0800

    Update: test_same_luck to test keyboard input

--- code/test_logic.py
+++ code/test_logic.py
@@ -1,14 +1,15 @@
 import logic
 from data_structures import Date
 import unittest
+from unittest.mock import patch
 
 class TestLogic(unittest.TestCase):
     def test_same_luck(self):
-        birthday1 = [Date(9, 7, 2005),
-                     Date(9, 7, 2005)]
+        birthday1 = [(9, 7, 2005),
+                     (9, 7, 2005)]
         
-        birthday2 = [Date(8, 8, 2005),
-                     Date(8, 9, 2005)]
+        birthday2 = [(8, 8, 2005),
+                     (8, 9, 2005)]
         
         expected = [True, False]
 
@@ -16,7 +17,11 @@ class TestLogic(unittest.TestCase):
                "Case: Lucky number of birthday1 != equal to lucky number of birthday 2"]
         
         for i in range(len(expected)):
-            self.assertEqual(expected[i], logic.same_luck(birthday1[i], birthday2[i]), msg[i])
+            with patch('builtins.input', side_effect=[str(x) for x in (*birthday1[i], *birthday2[i])]):
+                birthday1 = Date()
+                birthday2 = Date()
+                result = logic.same_luck(birthday1, birthday2)
+                self.assertEqual(expected[i], result, msg[i])
 
     def test_is_master_number(self):
         lucky_number = [11, 22, 33, 20, 14, 43]
