0910afc8259a695f9866e19f6e8445d3f93ac9b1
Komes Pispol <90035701@study.curtincollege.edu.au>
2025-05-03 16:29:57 +0800

    Update: lucky number to take keyboard input instead

--- code/calculator.py
+++ code/calculator.py
@@ -1,7 +1,8 @@
 from data_structures import Date
 import logic
 
-def calculate_lucky_number(birthday: Date):  
+def calculate_lucky_number():
+    birthday = Date()  
     date_string = str(birthday.day) + str(birthday.month) + str(birthday.year)
     lucky_number = 0
     for char in date_string:
