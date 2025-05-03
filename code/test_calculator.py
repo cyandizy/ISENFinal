b1f9cd56d93709e31af6f8ea7e30bb597d6328b0
Komes Pispol <90035701@study.curtincollege.edu.au>
2025-05-03 11:02:55 +0800

    Update: implemented BVA for test_get_generation

--- code/test_calculator.py
+++ code/test_calculator.py
@@ -70,3 +70,31 @@ class TestCalculator(unittest.TestCase):
         
         for i in range(len(expected)):
             self.assertEqual(expected[i], calculator.get_generation(year[i]), msg[i])
+
+
+    def test_get_generation_bva(self):
+        year_lower = [1945, 1964, 1979, 1994, 2009]
+        year_upper = [1946, 1965, 1980, 1995, 2010]
+        expected_lower = [
+            "Silent Generation",
+            "Baby Boomers",
+            "Generation X",
+            "Millennials",
+            "Generation Z"]
+        
+        expected_upper = [
+            "Baby Boomers",
+            "Generation X",
+            "Millennials",
+            "Generation Z",
+            "Generation Alpha"]
+
+        msg = ["Silent Generation / Baby Boomers",
+                    "Baby Boomers / Generation X",
+                    "Generation X / Millennials",
+                    "Millennials / Generation Z",
+                    "Generation Z / Generation Alpha"]
+        
+        for i in range(len(expected_lower)):
+            self.assertEqual(expected_lower[i], calculator.get_generation(year_lower[i]), f"[Lower] {msg[i]}")
+            self.assertEqual(expected_upper[i], calculator.get_generation(year_upper[i]), f"[Upper] {msg[i]}")
