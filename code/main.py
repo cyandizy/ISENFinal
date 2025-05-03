1a30ffae2fa5627d096e73350fd9076024d4111f
Komes Pispol <90035701@study.curtincollege.edu.au>
2025-05-03 17:06:36 +0800

    Add: implemented main.py

--- code/main.py
+++ code/main.py
@@ -2,27 +2,6 @@ import calculator
 import logic
 from data_structures import Date
 
-def prompt_date(prompt_message):
-    print(prompt_message)
-    valid_input = False
-    while not valid_input:
-        day = input("Input day (integer)> ")
-        month: str = input("Input month (integer or string)> ")
-        year = input("Input year (string)> ")
-        
-        try:
-            day = int(day)
-            if month.isdigit():
-                month = int(month)
-            year = int(year)
-
-            date = Date(day, month, year)
-            valid_input = True
-
-        except ValueError as e:
-            print(f"One of the input was invalid, please try again... {e}")
-    return date
-
 if __name__ == "__main__":
     welcome_message = """
                         Which functionality would you like to use?
@@ -33,42 +12,46 @@ if __name__ == "__main__":
     """
     print(welcome_message)
     valid_input = False
-    while not valid_input:
-        input_scenario = input("Functionality A/B/C> ")
-        if input_scenario[0].upper() == "A":
-            birthday = prompt_date("Please enter your birthday.")
+    while True:
+        input_scenario = input("Functionality A/B/C> ").strip().upper()
+        choice = input_scenario[0]
+        if choice == "A":
+            print("Please enter your birthday.")
+            birthday = Date()
             generation = calculator.get_generation(birthday.year)
             print(f"You belong to {generation}.")
             print()
             lucky_number = calculator.calculate_lucky_number(birthday)
             if logic.is_master_number(lucky_number):
-                print("Congratuation! Your lucky number is a master number!")
+                print("Congratuations! Your lucky number is a master number!")
             lucky_animal = calculator.get_lucky_animal(lucky_number)
             print()
             print(f"Your lucky number is {lucky_number},")
             print(f"and animal is {lucky_animal}.")
 
-            valid_input = True
+            break
         
-        elif input_scenario[0].upper() == "B":
-            birthday1 = prompt_date("Please enter birthday 1.")
-            print()
-            birthday2 = prompt_date("Please enter birthday 2.")
+        elif choice == "B":
+            print("Please enter birthday 1.")
+            birthday1 = Date()
+            print("Please enter birthday 2.")
+            birthday2 = Date()
             if logic.same_luck(birthday1, birthday2):
                 number = calculator.calculate_lucky_number(birthday1)
-                animal = calculator.get_lucky_animal(birthday1)
+                animal = calculator.get_lucky_animal(number)
                 print(f"Two birthdays have the same luck! You both have number {number} and {animal}")
             else:
                 print("Unfortunately, two birthdays do not have the same luck.")
 
-            valid_input = True
+            break
 
-        elif input_scenario[0].upper() == "C":
-            birthday = prompt_date("Please enter your birthday.")
+        elif choice == "C":
+            print("Please enter your birthday.")
+            birthday = Date()
             generation = calculator.get_generation(birthday.year)
             print(f"You belong to {generation}.")
 
-            valid_input = True
+            break
         
         if not valid_input:
             print("Your input is invalid, please try again.")
